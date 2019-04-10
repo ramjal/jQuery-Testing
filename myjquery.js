a5bcb5a6682ef24d53554dabfa6673c4c3fe6c2a
rjalali <ramjal@gmail.com>
2019-04-10 15:29:37 -0700

    more jquery stuff added

--- myjquery.js
+++ myjquery.js
@@ -38,7 +38,9 @@ $(document).ready(function () {
     });
 
     $("input").blur(function () {
+        var txt = $(this).val();
         $(this).css("background-color", "#ffffff");
+        $("#inputData").html(txt);
     });
 
     //multipe event on an element
@@ -56,9 +58,35 @@ $(document).ready(function () {
 
     //slider
     $("p.up").dblclick(function () {
-        $("div.down").slideToggle("slow", function() {
+        $("div.down").slideToggle("slow", function () {
             $("p.up").html("I just did that. didn't I?");
         });
+    });
+
+    //animation
+    $("#animate").click(function () {
+        $("#stopAnimate").css("visibility", "visible");        
+        for (var i = 0; i < 10; i++) {
+            $(this).animate({
+                left: "+=100px",
+                width: "+=300",
+                fontSize: "+=2em",
+                opacity: "0.1"
+            }, 1000);
+            $("#stopAnimate").animate({left: "500px"}, 1000);
+            $(this).animate({
+                left: "-=100px",
+                width: "-=300",
+                fontSize: "-=2em",
+                opacity: "1"
+            }, 1000);
+            $("#stopAnimate").animate({left: "0px"}, 1000);
+        }
+    });
+
+    $("#stopAnimate").click(function() {
+        $("this").stop(true);
+        $("#animate").stop(true);
     });
 
 });
